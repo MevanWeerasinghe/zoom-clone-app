0a128954b136b24682f6de8f5088bd0a9e1a69c9
Mevan Weerasinghe <marvolotrigg@gmail.com>
2024-05-17 23:36:31 +0530

    MeetingCard

--- app/(root)/(home)/upcoming/page.tsx
+++ app/(root)/(home)/upcoming/page.tsx
@@ -1,9 +1,11 @@
+import Calllist from "@/components/Calllist";
 import React from "react";
 
 const Upcoming = () => {
   return (
     <div className="flex size-full flex-col gap-10 text-white">
       <h1 className="text-3xl font-semibold">Upcoming</h1>
+      <Calllist type="upcoming" />
     </div>
   );
 };
--- components/Calllist.tsx
+++ components/Calllist.tsx
@@ -1,9 +1,55 @@
-import React from "react";
+"use client";
 
-const Calllist = () => {
+import { useGetCalls } from "@/hooks/useGetCalls";
+import { Call, CallRecording } from "@stream-io/video-react-sdk";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import MeetingCard from "./MeetingCard";
+
+const Calllist = ({ type }: { type: "upcoming" | "ended" | "recording" }) => {
+  const { upcomingCalls, endedCalls, callRecordings, isLoading } =
+    useGetCalls();
+  const router = useRouter();
+  const [recording, setRecording] = useState<CallRecording[]>([]);
+
+  const getCalls = () => {
+    switch (type) {
+      case "upcoming":
+        return upcomingCalls;
+      case "ended":
+        return endedCalls;
+      case "recording":
+        return callRecordings;
+      default:
+        return [];
+    }
+  };
+
+  const getNoCallsMessages = () => {
+    switch (type) {
+      case "upcoming":
+        return "No upcoming calls";
+      case "ended":
+        return "No ended calls";
+      case "recording":
+        return "No recordings";
+      default:
+        return "";
+    }
+  };
+
+  const calls = getCalls();
+  const noCallsMessage = getNoCallsMessages();
+
   return (
-    <div>
-      lol<div>Upcomming</div>
+    <div className="grid grid-cols-1 gap-5 xl:grid-cols-2">
+      {calls && calls.length > 0 ? (
+        calls.map((meeting: Call | CallRecording) => {
+          return <div></div>; //<MeetingCard />;
+        })
+      ) : (
+        <p>{noCallsMessage}</p>
+      )}
     </div>
   );
 };
